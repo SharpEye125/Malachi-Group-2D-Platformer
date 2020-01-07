64214b10571dcff34c63f7cf902ebae961f0de3a
Malachi Weis <21.malachi.weis@ksd.org>
2020-01-07 12:44:57 -0800

    Put in some scripts

--- Group2DPlatformer/Assets/Scripts/MeleePlayerAttack.cs
+++ Group2DPlatformer/Assets/Scripts/MeleePlayerAttack.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MeleePlayerAttack : MonoBehaviour
 {
+    public Animator Animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public int attackDamage = 1;
+    public LayerMask enemyLayers;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,24 @@ public class MeleePlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+    void Attack()
+    {
+        Animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
