39e3f5da819ac9bf7f47cc287fec399e6ac59b8e
Victor Hernandez <20.victor.hernandez@ksd.org>
2020-01-10 13:54:21 -0800

    added scripts

---
+++ Group2DPlatformer/Assets/Scripts/VhScripts/GameTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    public float levelTime = 120.0f;
+    float timer = 0;
+    public Text TimeText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeText.text = "Time: " + timer + "/100";
+        timer += Time.deltaTime;
+        if(timer > levelTime)
+        {
+            SceneManager.LoadScene(
+                SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Group2DPlatformer/Assets/Scripts/VhScripts/MainMenu2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu2 : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("LevelOne");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
